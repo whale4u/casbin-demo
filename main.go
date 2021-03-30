c23b22c387879c1c4a8525b29cc049629ff9b96e
whale4u <whale4u@hotmail.com>
2021-03-30 23:53:20 +0800

    first commit

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"github.com/casbin/casbin/v2"
 )
 
 func main() {
 	e, err := casbin.NewEnforcer("./model.conf", "./policy.csv")
 
-	sub := "zhangsan" // the user that wants to access a resource.
-	obj := "data1"    // the resource that is going to be accessed.
-	act := "read"     // the operation that the user performs on the resource.
+	sub := "susan" // the user that wants to access a resource.
+	obj := "data1" // the resource that is going to be accessed.
+	act := "read"  // the operation that the user performs on the resource.
+
+	//add susan
+	added, err := e.AddPolicy("susan", "data1", "read")
+	fmt.Println(added)
+	fmt.Println(err)
 
 	ok, err := e.Enforce(sub, obj, act)
 
