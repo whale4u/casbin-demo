c4c617fbf9473bcb2fb17ed092e73894942e510c
whale4u <whale4u@hotmail.com>
2021-03-31 23:39:24 +0800

    rbac model

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # casbin-demo
 - feature/v1 基本实现casbin
 - feature/v2 实现基于gorm+mysql的casbin增删查改
+- feature/v3 相较于v2增加model角色概念
--- main.go
+++ main.go
@@ -17,25 +17,13 @@ func main() {
 	e.LoadPolicy()
 
 	sub := "susan" // the user that wants to access a resource.
-	obj := "data1" // the resource that is going to be accessed.
-	act := "read"  // the operation that the user performs on the resource.
+	obj := "data2" // the resource that is going to be accessed.
+	act := "write" // the operation that the user performs on the resource.
 
-	//add susan
-	added, err := e.AddPolicy("susan", "data2", "read")
+	//增加susan为data2数据的admin角色
+	added, err := e.AddGroupingPolicy("susan", "data2_admin")
 	fmt.Println(added)
 	fmt.Println(err)
-
-	//find susan
-	filteredPolicy := e.GetFilteredPolicy(0, "susan")
-	fmt.Println(filteredPolicy)
-
-	//update susan
-	updated, err := e.UpdatePolicy([]string{"susan", "data2", "read"}, []string{"susan", "data2", "write"})
-	fmt.Println(updated)
-
-	//delete susan
-	removed, err := e.RemovePolicy("susan", "data2", "write")
-	fmt.Println("remove susan:", removed)
 
 	ok, err := e.Enforce(sub, obj, act)
 
