7b0bcf08a7c579b8325efb932738d1544d963692
whale4u <whale4u@hotmail.com>
2021-04-07 00:24:32 +0800

    fix bugs

--- main.go
+++ main.go
@@ -11,8 +11,10 @@ import (
 func main() {
 	a, _ := gormadapter.NewAdapter("mysql", "root:@tcp(127.0.0.1:3306)/casbin", true) // Your driver and data source.
 	e, _ := casbin.NewEnforcer("./model.conf", a)
-	e.AddFunction("my_func", KeyMatchFunc)
 	// e, err := casbin.NewEnforcer("./model.conf", "./policy.csv")
+
+	// 注册自定义函数
+	// e.AddFunction("my_func", KeyMatchFunc)
 
 	// Load the policy from DB.
 	e.LoadPolicy()
@@ -21,12 +23,15 @@ func main() {
 	obj := "data2" // the resource that is going to be accessed.
 	act := "write" // the operation that the user performs on the resource.
 
+	_, err := e.AddPolicy("admin", "data2", "read")
 	//增加susan为data2数据的admin角色
-	added, err := e.AddGroupingPolicy("susan", "data2_admin")
+	added, err := e.AddGroupingPolicy("susan", "admin")
 	fmt.Println(added)
 	fmt.Println(err)
 
 	ok, err := e.Enforce(sub, obj, act)
+
+	// fmt.Println(ok)
 
 	if err != nil {
 		// handle err
@@ -46,9 +51,9 @@ func KeyMatch(key1 string, key2 string) bool {
 	return key1 == key2
 }
 
-func KeyMatchFunc(args ...interface{}) (interface{}, error) {
-	name1 := args[0].(string)
-	name2 := args[1].(string)
+// func KeyMatchFunc(args ...interface{}) (interface{}, error) {
+// 	name1 := args[0].(string)
+// 	name2 := args[1].(string)
 
-	return (bool)(KeyMatch(name1, name2)), nil
-}
+// 	return (bool)(KeyMatch(name1, name2)), nil
+// }
