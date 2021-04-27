fb2bfc18ae2e2e32ae204dacc7bc9b9fb57f56b7
whale4u <whale4u@hotmail.com>
2021-04-27 20:39:34 +0800

    feature/v5

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - feature/v2 实现基于gorm+mysql的casbin增删查改
 - feature/v3 相较于v2增加model角色概念
 - feature/v4 相较于v3增加自定义函数
+- feature/v5 相较于v2增加keyMatch函数
--- main.go
+++ main.go
@@ -9,29 +9,29 @@ import (
 )
 
 func main() {
-	a, err := gormadapter.NewAdapter("mysql", "root:@tcp(127.0.0.1:3306)/casbin", true) // Your driver and data source.
-	fmt.Println(err)
-	e, _ := casbin.NewEnforcer("./model.conf", a)
-	// e, err := casbin.NewEnforcer("./model.conf", "./policy.csv")
+	//a, err := gormadapter.NewAdapter("mysql", "root:@tcp(127.0.0.1:3306)/casbin", true) // Your driver and data source.
+	//fmt.Println(err)
+	//e, _ := casbin.NewEnforcer("./model.conf", a)
+	e, err := casbin.NewEnforcer("./rbac_model.conf", "./rbac_policy.csv")
 
 	// 注册自定义函数
-	e.AddFunction("my_func", KeyMatchFunc)
+	//e.AddFunction("my_func", KeyMatchFunc)
 
 	// Load the policy from DB.
-	e.LoadPolicy()
+	//e.LoadPolicy()
 
-	sub := "susan" // the user that wants to access a resource.
-	obj := "data1" // the resource that is going to be accessed.
-	act := "read"  // the operation that the user performs on the resource.
+	sub := "alice"      // the user that wants to access a resource.
+	obj := "/admin/xxx" // the resource that is going to be accessed.
+	act := "GET"        // the operation that the user performs on the resource.
 
-	_, err1 := e.AddPolicy("data2_admin", "data2", "read")
-	fmt.Println(err1)
-	_, err2 := e.AddPolicy("data2_admin", "data2", "write")
-	fmt.Println(err2)
-	//增加susan为data2数据的admin角色
-	added, err := e.AddGroupingPolicy("susan", "data2_admin")
-	fmt.Println(added)
-	fmt.Println(err)
+	//_, err1 := e.AddPolicy("data2_admin", "data2", "read")
+	//fmt.Println(err1)
+	//_, err2 := e.AddPolicy("data2_admin", "data2", "write")
+	//fmt.Println(err2)
+	////增加susan为data2数据的admin角色
+	//added, err := e.AddGroupingPolicy("susan", "data2_admin")
+	//fmt.Println(added)
+	//fmt.Println(err)
 
 	ok, err := e.Enforce(sub, obj, act)
 
@@ -49,15 +49,4 @@ func main() {
 		// deny the request, show an error
 		fmt.Println(sub, obj, act, " Fail")
 	}
-}
-
-func KeyMatch(key1 string, key2 string) bool {
-	return key1 == key2
-}
-
-func KeyMatchFunc(args ...interface{}) (interface{}, error) {
-	name1 := args[0].(string)
-	name2 := args[1].(string)
-
-	return (bool)(KeyMatch(name1, name2)), nil
 }
