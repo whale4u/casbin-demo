5c144e34eba1445c94e4797c7bdcf31eadffec12
whale4u <whale4u@hotmail.com>
2021-05-24 23:55:39 +0800

    fix bugs

--- README.md
+++ README.md
@@ -3,4 +3,10 @@
 - feature/v2 实现基于gorm+mysql的casbin增删查改
 - feature/v3 相较于v2增加model角色概念
 - feature/v4 相较于v3增加自定义函数
-- feature/v5 相较于v2增加keyMatch函数
+- feature/v5 实现RBAC版本casbin
+
+## V5 RBAC Casbin
+```
+go run main.go
+alice /data2_admin/xxx GET  Pass
+```
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"github.com/casbin/casbin/v2"
-	gormadapter "github.com/casbin/gorm-adapter/v3"
+	// gormadapter "github.com/casbin/gorm-adapter/v3"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -20,9 +20,9 @@ func main() {
 	// Load the policy from DB.
 	//e.LoadPolicy()
 
-	sub := "alice"      // the user that wants to access a resource.
-	obj := "/admin/xxx" // the resource that is going to be accessed.
-	act := "GET"        // the operation that the user performs on the resource.
+	sub := "alice"            // the user that wants to access a resource.
+	obj := "/data2_admin/xxx" // the resource that is going to be accessed.
+	act := "GET"              // the operation that the user performs on the resource.
 
 	//_, err1 := e.AddPolicy("data2_admin", "data2", "read")
 	//fmt.Println(err1)
