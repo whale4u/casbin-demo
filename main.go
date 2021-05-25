c26702b769a02011f034826539e3542064b2bf59
whale4u <whale4u@hotmail.com>
2021-05-25 19:21:20 +0800

    fix bug

--- main.go
+++ main.go
@@ -2,33 +2,49 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/casbin/casbin/v2"
 )
 
 func main() {
-	e, err := casbin.NewEnforcer("./model.conf", "./policy.csv")
+	// 新建1个文件适配器
+	e, err := casbin.NewEnforcer("model.conf", "policy.csv")
 
 	sub := "susan" // the user that wants to access a resource.
 	obj := "data1" // the resource that is going to be accessed.
 	act := "read"  // the operation that the user performs on the resource.
 
-	//add susan
-	added, err := e.AddPolicy("susan", "data1", "read")
-	fmt.Println(added)
-	fmt.Println(err)
+	ok1, err := e.Enforce(sub, obj, act)
+	if err != nil {
+		// handle err
+		fmt.Printf("%s", err)
+	}
 
-	ok, err := e.Enforce(sub, obj, act)
+	if ok1 {
+		// permit alice to read data1
+		fmt.Println("check pass")
+	} else {
+		// deny the request, show an error
+		fmt.Println("check fail")
+	}
+
+	// 新增1条policy
+	added, err := e.AddPolicy("susan", "data1", "read")
+	fmt.Println("added operation: ", added)
+	// fmt.Println(err)
+
+	ok2, err := e.Enforce(sub, obj, act)
 
 	if err != nil {
 		// handle err
 		fmt.Printf("%s", err)
 	}
 
-	if ok == true {
+	if ok2 {
 		// permit alice to read data1
-		fmt.Println("Pass")
+		fmt.Println("check pass")
 	} else {
 		// deny the request, show an error
-		fmt.Println("Fail")
+		fmt.Println("check fail")
 	}
 }
